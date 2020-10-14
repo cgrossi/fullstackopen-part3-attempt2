aa2dbe70f61b2f2ef4bbdd4592fb3ce4c6f78846
cgrossi <cgrossi@gmail.com>
2020-10-13 22:18:00 -0600

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const morgan = require('morgan');
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token('data', (req, res) => {
@@ -104,7 +105,7 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`)
 })
